af36537b140b79297df8a8c9533047345fab4e46
842mono <842mono@gmail.com>
2019-10-30 23:19:02 -0400

    cleanup

--- Problem 2/Part2.py
+++ Problem 2/Part2.py
@@ -12,51 +12,12 @@ def memoize_aspect(f, *args, **kwargs):
         yield Return(memo[key])
     yield Return(memo[key])
 
-
-
 def weave_memoize(source, Globals):
-
-    # all_functions = inspect.getmembers(inspect.getmodulename("__module__"), inspect.isfunction)
-    # print(all_functions)
-    # for e in all_functions:
-    #     print(e)
-
-    # for e in dir(__name__):
-    #     print(e)
-
-#     filename = sys.argv[1]
-#     with open(filename, 'r') as code_file:
-#         code = code_file.read()
-#         print(code)
-#         code = weave(code, memoize_aspect)
-#         code()
-
-
 
     tree = ast.parse(source)
 
-    # statements = []
     for statement in tree.body:
         if isinstance(statement, ast.FunctionDef) and not (statement.name == "memoize_aspect" or statement.name == "weave_memoize"):
-            print("ddd")
-            print(statement.name)
-            print(inspect.getsource(Globals[statement.name]))
+            # print(inspect.getsource(Globals[statement.name]))
             weave(Globals[statement.name], memoize_aspect)
-            print(inspect.getsource(Globals[statement.name]))
-            print("dbg")
-            # print(statement)
-            # for e in dir(statement):
-            #     print(e)
-            # weave(statement, memoize_aspect)
-            # print(statement)
-        # statements.append(statement)
-    # tree.body = statements
-    # print(tree.body)
-    # code = astor.to_source(tree)
-    
-    # print('# output code --------------------------')
-    # print(code)
-    # print('# --------------------------------------')
-
-    # exec(code)
-    print(memo)
+            # print(inspect.getsource(Globals[statement.name]))
--- Problem 2/test2.py
+++ Problem 2/test2.py
@@ -1,6 +1,5 @@
-from Part2 import weave_memoize, memo
+from Part2 import weave_memoize #, memo
 import inspect, sys
-# inspect.getsource(sys.modules[__name__])
 
 def fib(n):
     if n == 0:
@@ -10,9 +9,7 @@ def fib(n):
 
     return fib(n-1) + fib(n-2)
 
-# print(globals()["fib"])
 weave_memoize(inspect.getsource(sys.modules[__name__]), globals())
-# print(dir("__main__"))
 
 print(fib(5))
-print(memo)
+# print(memo)
